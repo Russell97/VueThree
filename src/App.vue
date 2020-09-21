d17fcb2232b15a492f6a90e6c06cfff3adcb78de
liwenjing <15119285499@163.com>
2020-09-21 23:29:55 +0800

    09212330

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+  setup () {
+    onMounted(() => {
+      console.log('挂载后')
+      console.log(process.env.NODE_ENV)
+    })
+  }
+}
+</script>
+
+<style lang="scss"></style>
